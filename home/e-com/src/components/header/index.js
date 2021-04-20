030786863d14043b6f9a8fbfd10afa48ba8e6735
daksh890 <57205074+daksh890@users.noreply.github.com>
2021-04-20 09:20:01 +0530

    Sign In with google/ Login-Logout

--- home/e-com/src/components/header/index.js
+++ home/e-com/src/components/header/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.scss';
 import { Link } from 'react-router-dom';
+import { auth } from './../../firebase/utils';
 
 
 import logo from './../../assets/logo.png'
 
 const Header = props => {
+    const { currentUser } = props;
     return(
         <header className="header">
             <div className="wrap">
@@ -16,17 +18,41 @@ const Header = props => {
                 </div>
 
                 <div className="callToActions">
+
+                    {currentUser && (
+                        <ul>
+                            <li>
+                                <span onClick={() =>auth.signOut()}>
+                                    LogOut
+                                </span>
+                            </li>
+                        </ul>
+                    )}
+
+                    
+                    {!currentUser &&(
                     <ul>
                         <li>
                             <Link to ="/registration">
                                 Register
                             </Link>
                         </li>
+
+                        <li>
+                            <Link to ="/login">
+                                Login
+                            </Link>
+                        </li>
                     </ul>
+                    )}
                 </div>
             </div>
         </header>
     );
 };
+
+Header.defaultProps ={
+    currentUser: null,
+}
 
 export default Header;
