dd727171706ee30a52aa24ef3257c17f5b501a20
daksh890 <57205074+daksh890@users.noreply.github.com>
2021-05-13 18:46:48 +0530

    Nothing

--- home/e-com/src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyB4jrw7SnG_LELG7K9g19PH0FVOKUxOPmA",
-    authDomain: "personal-distributor.firebaseapp.com",
-    projectId: "personal-distributor",
-    storageBucket: "personal-distributor.appspot.com",
-    messagingSenderId: "759678117267",
-    appId: "1:759678117267:web:79f25836947f2263463466",
-    measurementId: "G-42CN9L3CW7"
-  }; 
